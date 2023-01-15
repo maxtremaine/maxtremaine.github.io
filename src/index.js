9cdb08f18b2587bfc60073841d09fbc4471ebc04
maxtremaine <max.tremaine@gmail.com>
2023-01-15 00:19:07 -0500

    Added spacing.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ $.getJSON('src/data.json', function(json) {
     const data = json.maxtremaine.data
     console.log(data)
     $('.loaded').toggle()
-    Object.values(data.contact).forEach(function(contact) {
-        $(`<li><a href="${contact.link}">${contact.title}</a></li>`).appendTo('#contactList')
-    })
-    Object.values(data.articles).sort(function(a, b) { return a.order - b.order })
+    Object.values(data.contact)
+        .forEach(function(contact) {
+            $(`<li><a href="${contact.link}">${contact.title}</a></li>`).appendTo('#contactList')
+        })
+    Object.values(data.articles)
+        .sort(function(a, b) { return a.order - b.order })
         .forEach(function(article) {
             $(`<li><a href="${article.link}" target="_blank">${article.title}</a>: ${article.description} (${article.date})</li>`)
                 .appendTo('#articleList')
