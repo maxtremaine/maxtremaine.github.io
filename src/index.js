d801e4ada3897120559d88b8a56e457ae0382c90
maxtremaine <max.tremaine@gmail.com>
2023-01-15 00:52:29 -0500

    Added factory functions.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+function createContactListItem(contact) {
+    return '<li><a href="' + contact.link + '" target="_blank">' + contact.title + '</a></li>'
+}
+
+function createArticleListItem(article) {
+    return '<li><a href="' + article.link + '" target="_blank">' + article.title + '</a>:' + article.description + '(' + article.date + ')</li>'
+}
+
 $.getJSON('src/data.json', function(data) {
-    console.log(data)
     data = data.maxtremaine.data // Website content.
-    $('.loaded').toggle()
+    $('#scriptWarning').toggle()
+    $('.loaded').toggle() // Show containers for loaded data.
     Object.values(data.contact)
         .forEach(function(contact) {
-            $(`<li><a href="${contact.link}">${contact.title}</a></li>`).appendTo('#contactList')
+            $(createContactListItem(contact)).appendTo('#contactList')
         })
     Object.values(data.articles)
         .sort(function(a, b) { return a.order - b.order })
         .forEach(function(article) {
-            $(`<li><a href="${article.link}" target="_blank">${article.title}</a>: ${article.description} (${article.date})</li>`)
-                .appendTo('#articleList')
+            $(createArticleListItem(article)).appendTo('#articleList')
         })
-    $('#scriptWarning').toggle()
 })
