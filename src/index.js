bc79db33855c2e602d89b2c76e34744c2a36f4f8
maxtremaine <max.tremaine@gmail.com>
2023-01-15 00:18:23 -0500

    Added articles.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,10 @@ $.getJSON('src/data.json', function(json) {
     Object.values(data.contact).forEach(function(contact) {
         $(`<li><a href="${contact.link}">${contact.title}</a></li>`).appendTo('#contactList')
     })
+    Object.values(data.articles).sort(function(a, b) { return a.order - b.order })
+        .forEach(function(article) {
+            $(`<li><a href="${article.link}" target="_blank">${article.title}</a>: ${article.description} (${article.date})</li>`)
+                .appendTo('#articleList')
+        })
     $('#scriptWarning').toggle()
 })
