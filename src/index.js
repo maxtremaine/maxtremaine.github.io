32fe611e50aaa03cb66e2881af39fde5dc83d19e
Max Tremaine <max.tremaine@gmail.com>
2024-01-05 20:19:00 -0500

    Get rid of jQuery

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ function createArticleListItem(article) {
     return '<li><a href="' + article.link + '" target="_blank">' + article.title + '</a>: ' + article.description + ' (' + article.date + ')</li>'
 }
 
-$.getJSON('src/data.json', function(data) {
+function renderPage(data) {
     data = data.maxtremaine.data // Website content.
-    $('#scriptWarning').toggle()
-    $('.loaded').toggle() // Show containers for loaded data.
+    document.getElementById('scriptWarning').style.display = 'none'
+    for(const element of document.getElementsByClassName('loaded')){
+        element.style.display = 'block'
+    }
     Object.values(data.contact)
         .forEach(function(contact) {
-            $(createContactListItem(contact)).appendTo('#contactList')
+            document.getElementById('contactList').innerHTML +=createContactListItem(contact)
         })
     Object.values(data.articles)
         .sort(function(a, b) { return a.order - b.order })
         .forEach(function(article) {
-            $(createArticleListItem(article)).appendTo('#articleList')
+            document.getElementById('articleList').innerHTML += createArticleListItem(article)
         })
-})
+}
+
+function loadPage() {
+    fetch('src/data.json')
+        .then(data => data.json())
+        .then(renderPage)
+}
