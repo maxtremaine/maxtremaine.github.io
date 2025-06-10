17025eca873eece9480be0753ef0887bb3dc1966
Maximilian Tremaine <max@Maximilians-MacBook-Pro.local>
2025-06-10 14:17:29 +0100

    Make parameters required

--- test/data.test.js
+++ test/data.test.js
@@ -10,35 +10,35 @@ describe("types", () => {
   });
   it("Should validate contacts", () => {
     for (const contact of Object.values(data.maxtremaine.data.contact)) {
-      for (const key in contact) {
-        chai.assert.equal(types.book[key], typeof contact[key]);
+      for (const key in types.contact) {
+        chai.assert.equal(types.contact[key], typeof contact[key]);
       }
     }
   });
   it("Should validate articles", () => {
     for (const article of Object.values(data.maxtremaine.data.articles)) {
-      for (const key in article) {
+      for (const key in types.article) {
         chai.assert.equal(types.article[key], typeof article[key]);
       }
     }
   });
   it("Should validate books", () => {
     for (const book of Object.values(data.maxtremaine.data.favouriteBooks)) {
-      for (const key in book) {
+      for (const key in types.book) {
         chai.assert.equal(types.book[key], typeof book[key]);
       }
     }
   });
   it("Should validate quotes", () => {
     for (const quote of Object.values(data.maxtremaine.data.favouriteQuotes)) {
-      for (const key in quote) {
+      for (const key in types.quote) {
         chai.assert.equal(types.quote[key], typeof quote[key]);
       }
     }
   });
   it("Should validate resume", () => {
     for (const job of Object.values(data.maxtremaine.data.resume)) {
-      for (const key in job) {
+      for (const key in types.resume) {
         chai.assert.equal(types.resume[key], typeof job[key]);
       }
     }
