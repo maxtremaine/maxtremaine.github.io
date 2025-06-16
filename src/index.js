372da4f91d6ed69e4cecf1096b0644bf7e651b71
Max Tremaine <max.tremaine@gmail.com>
2025-06-16 15:40:43 -0400

    Update style to match Zig

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 // Render HTML from data.
 
-const renderContactListItem = contact => (
-    `<li><a href="${contact.link}" target="_blank">${contact.title}</a></li>`
-)
+const renderContactListItem = (contact) =>
+  `<li><a href="${contact.link}" target="_blank">${contact.title}</a></li>`;
 
-const renderArticleListItem = article => (
-    `<li><a href="${article.link}" target="_blank">${article.title}</a>: ${article.description} (${article.date})</li>`
-)
+const renderArticleListItem = (article) =>
+  `<li><a href="${article.link}" target="_blank">${article.title}</a>: ${article.description} (${article.date})</li>`;
 
-const renderQuoteListItem = favouriteQuote => (
-    `<li>"${favouriteQuote.quote}" -${favouriteQuote.author}</li>`
-)
+const renderQuoteListItem = (favouriteQuote) =>
+  `<li>"${favouriteQuote.quote}" -${favouriteQuote.author}</li>`;
 
 // Handle data and rendering.
 
-document.renderMTDC = data => {
-    data = data.maxtremaine.data // Website content.
-    document.querySelector('#scriptWarning').style.display = 'none'
-    document.querySelector('#loadedContent').style.display = 'block'
-    document.querySelector('#contactList').innerHTML = Object.values(data.contact)
-        .map(renderContactListItem)
-        .join('')
-    document.querySelector('#articleList').innerHTML = Object.values(data.articles)
-        .sort((a, b) => a.order - b.order )
-        .map(renderArticleListItem)
-        .join('')
-    document.querySelector('#quoteList').innerHTML = Object.values(data.favouriteQuotes)
-        .filter(quote => quote.public)
-        .sort((a, b) => a.length - b.length)
-        .map(renderQuoteListItem)
-        .join('')
-}
+document.renderMTDC = (data) => {
+  data = data.maxtremaine.data; // Website content.
+  document.querySelector("#scriptWarning").style.display = "none";
+  document.querySelector("#loadedContent").style.display = "block";
+  document.querySelector("#contactList").innerHTML = Object.values(data.contact)
+    .map(renderContactListItem)
+    .join("");
+  document.querySelector("#articleList").innerHTML = Object.values(
+    data.articles,
+  )
+    .sort((a, b) => a.order - b.order)
+    .map(renderArticleListItem)
+    .join("");
+  document.querySelector("#quoteList").innerHTML = Object.values(
+    data.favouriteQuotes,
+  )
+    .filter((quote) => quote.public)
+    .sort((a, b) => a.length - b.length)
+    .map(renderQuoteListItem)
+    .join("");
+};
 
-const loadAndRender = () => { // Set to run on body load.
-    fetch('src/data.json')
-        .then(data => data.json())
-        .then(document.renderMTDC)
-}
+const loadAndRender = () => {
+  // Set to run on body load.
+  fetch("src/data.json")
+    .then((data) => data.json())
+    .then(document.renderMTDC);
+};
